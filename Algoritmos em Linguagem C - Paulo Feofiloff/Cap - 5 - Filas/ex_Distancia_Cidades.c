30d296453b2fc2cd0382deeeaa8274d3ba2f624c
launzoa <75075230+launzoa@users.noreply.github.com>
2024-10-14 15:58:08 -0300

    Update ex_Distancia_Cidades.c
    Arrumando alguns erros

--- Algoritmos em Linguagem C - Paulo Feofiloff/Cap - 5 - Filas/ex_Distancia_Cidades.c
+++ Algoritmos em Linguagem C - Paulo Feofiloff/Cap - 5 - Filas/ex_Distancia_Cidades.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int **Matriz_Cidades(int n);
+int **Matriz_cidades(int n);
+
+void Imprime_matriz(int **matriz, int n);
 
 void Liberar(int **matriz, int n);
 
 int *Distancia_cidade(int **matriz, int cidade, int n);
 
+void Imprime_cidade(int *distancia, int cidade, int n);
 
 int main()
 {
@@ -16,20 +19,22 @@ int main()
     scanf("%d", &n);
     system("cls");
 
-    int **cidades = Matriz_Cidades(n);
+    int **cidades = Matriz_cidades(n);
 
-    printf("Entre com a cidade que você gostaria de analisar a distancia (baseado em numeros de cidades percorridas): ");
+    printf("Entre com a cidade que voce gostaria de analisar a distancia (baseado em numeros de cidades percorridas): ");
     scanf("%d", &k);
     system("cls");
 
     int *distancia = Distancia_cidade(cidades, k-1, n);
+
+    Imprime_cidade(distancia, k, n);
 
     Liberar(cidades, n);
 
     return 0;
 }
 
-int **Matriz_Cidades(int n)
+int **Matriz_cidades(int n)
 {
     int i, j, **matriz, c;
     matriz = malloc(n * sizeof(int*));
@@ -50,21 +55,30 @@ int **Matriz_Cidades(int n)
         matriz[c][c] = 1;
             
     }
+    
+    system("cls");
 
-    printf("|-------------|\n");
+    Imprime_matriz(matriz, n);
+
+    return matriz;
+}
+
+
+void Imprime_matriz(int **matriz, int n)
+{
+    int i, j;
+
+    printf("Tabela de conexoes das cidades:\n");
+    for(i = 0; i < n; i++) printf("\t     Cidade %d", i+1);
+    printf("\n");
     for(i = 0; i < n; i++)
-    {
-        
-        for(j = 0; j < n; j++)
-        {
-            printf("| %d |", matriz[i][j]);
-        }
+    {   
+        printf("Cidade %d", i);
+        for(j = 0; j < n; j++) printf("\t%d\t", matriz[i][j]);
         printf("\n");
     }
-    printf("|-------------|\n");
     system("pause");
     system("cls");
-    return matriz;
 }
 
 
@@ -87,18 +101,44 @@ int *Distancia_cidade(int **matriz, int cidade, int n)
     
     distancia[cidade] = 0;
 
-    i = 0;
-    int *fila = cidade;
+    i = 1;
+    int *fila = malloc((n+1) * sizeof(int));
+    *fila = cidade;
+    fila[n] = -1;
+    int k = 0;
 
-    while (fila != NULL)
+    while (fila[k] != -1)
     {
         for(j = 0; j < n; j++)
         {
-            if(matriz[*fila][j] == 1 && distancia[j] == -1)
+            if(matriz[fila[k]][j] == 1 && distancia[j] == -1)
             {
-                fila[i++] = j;
-                distancia[j] = distancia[*fila] + 1;
+                fila[i] = j;
+                i++;
+                distancia[j] = distancia[fila[k]] + 1;
             }
         }
+        k++;
     }
+
+    return distancia;
 }   
+
+
+void Imprime_cidade(int *distancia, int cidade, int n)
+{
+    printf("Tabela das cidades\nA distancia da cidade %d para as demais cidades\n", cidade);
+    
+    for(int i = 0; i < n; i++)
+    {
+        if(distancia[i] != -1)
+        {
+            printf("Cidade %d => Cidade %d, a distancia eh %d\n", cidade, i+1, distancia[i]);
+        }
+        else
+        {
+            printf("Não existe caminho da nossa Cidade %d para a Cidade %d", cidade, i+1);
+        }
+    } 
+    printf("\n");
+}
